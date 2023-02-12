5a1fd442730dee71048323e839c067d855432a83
Manuel <manutepowa@gmail.com>
2023-02-12 21:41:11 +0100

    Add suspense on page for load only the loading content

--- src/app/calendar/calendar2/page.tsx
+++ src/app/calendar/calendar2/page.tsx
@@ -1,3 +1,20 @@
-export default function Calendar2() {
-  return <h1>Calendar2</h1>
+import ListOfUsers from "@/components/ListOfUsers"
+import { ReactNode, Suspense } from "react"
+
+export default function Calendar2(): ReactNode {
+  return (
+    <div>
+      <h1>Probando suspense</h1>
+      <ul>
+        <li>
+          Este texto se verá en la pantalla mientras que el contenido del
+          componente que está cargando, se verá en loading
+        </li>
+      </ul>
+      <Suspense fallback={<p className="p-8 bg-yellow-700">Loading........</p>}>
+        {/* @ts-expect-error Server Component */}
+        <ListOfUsers />
+      </Suspense>
+    </div>
+  )
 }
