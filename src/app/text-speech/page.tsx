1cc2ea24bac78f9cc44553b05c466c39bbea06ad
Manuel <manutepowa@gmail.com>
2023-02-24 09:14:24 +0100

    Add new metadata

--- src/app/text-speech/page.tsx
+++ src/app/text-speech/page.tsx
@@ -1,3 +1,10 @@
+import type { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: "Speech recognition and text to speech",
+  description: "Speech recognition and text to speech",
+}
+
 export default function TextSpeechPage() {
   return (
     <div className="justify-center">
