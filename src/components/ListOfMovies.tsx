597771f1ebf3a2d16c2c821cc64a6c59a620ad26
Manuel <manutepowa@gmail.com>
2023-03-06 14:03:14 +0100

    Change boolean values to enums

--- src/components/ListOfMovies.tsx
+++ src/components/ListOfMovies.tsx
@@ -1,34 +1,32 @@
 "use client"
-import { MoviesContext, MovieType } from "@/context/MoviesContext"
+import { MoviesContext, MovieType, QueryState } from "@/context/MoviesContext"
 import { useContext } from "react"
 import Loading from "./Loading"
 
 export default function ListOfMovies() {
-  const { movies, query, loading } = useContext(MoviesContext)
-
+  const { movies, query, state } = useContext(MoviesContext)
+  console.log({ state })
   return (
     <div className="mt-8">
-      {loading ? (
-        <Loading />
-      ) : (
+      {(state === QueryState.INITIAL || query === "") && (
+        <h1>Escribe para buscar</h1>
+      )}
+      {state === QueryState.LOADING && <Loading />}
+      {state === QueryState.SUCCESS && (
         <div>
-          {query === "" ? (
-            <h1>Escribe para buscar</h1>
-          ) : (
-            <ul className="grid grid-cols-3 gap-4">
-              {movies.length > 0 ? (
-                movies.map((movie: MovieType) => (
-                  <li key={movie.imdbID}>
-                    <h3>{movie.title}</h3>
-                    <div>{movie.year}</div>
-                    <img src={movie.poster} alt={movie.title} />
-                  </li>
-                ))
-              ) : (
-                <div>No hay movies</div>
-              )}
-            </ul>
-          )}
+          <ul className="grid grid-cols-3 gap-4">
+            {movies.length > 0 ? (
+              movies.map((movie: MovieType) => (
+                <li key={movie.imdbID}>
+                  <h3>{movie.title}</h3>
+                  <div>{movie.year}</div>
+                  <img src={movie.poster} alt={movie.title} />
+                </li>
+              ))
+            ) : (
+              <div>No hay movies</div>
+            )}
+          </ul>
         </div>
       )}
     </div>
--- src/context/MoviesContext.tsx
+++ src/context/MoviesContext.tsx
@@ -16,12 +16,20 @@ export interface MovieType {
   poster: string
 }
 
+export const QueryState = {
+  INITIAL: "INITIAL",
+  LOADING: "LOADING",
+  SUCCESS: "SUCCESS",
+  ERROR: "ERROR",
+}
+export type StateType = (typeof QueryState)[keyof typeof QueryState]
+
 interface MoviesContextType {
   query: string
   handleSubmit: (e: React.FormEvent<HTMLFormElement>) => void
   movies: MovieType[]
   updateQuery: (e: React.ChangeEvent<HTMLInputElement>) => void
-  loading: boolean
+  state: StateType
 }
 
 const MoviesContext = createContext<MoviesContextType>({} as MoviesContextType)
@@ -29,16 +37,20 @@ const MoviesContext = createContext<MoviesContextType>({} as MoviesContextType)
 function MoviesProvider({ children }: { children: React.ReactNode }) {
   const [query, setQuery] = useState("")
   const [movies, setMovies] = useState<MovieType[]>([])
-  const [loading, setLoading] = useState<boolean>(false)
+  const [state, setState] = useState<StateType>("")
   // handlesubmit form
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     const { searchQuery } = e.target as typeof e.target & {
       searchQuery: { value: string }
     }
+    if (searchQuery.value === "") {
+      setState(QueryState.INITIAL)
+      return
+    }
     setQuery(searchQuery.value)
+    setState(QueryState.LOADING)
     onSearchMovies(searchQuery.value)
-    setLoading(true)
   }
 
   const onSearchMovies = useCallback(
@@ -50,21 +62,20 @@ function MoviesProvider({ children }: { children: React.ReactNode }) {
         .catch((err) => {
           console.log({ err })
         })
-        .finally(() => setLoading(false))
+        .finally(() => setState(QueryState.SUCCESS))
     }, 500),
     []
   )
   const updateQuery = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target
-    console.log({ value })
     setQuery(value)
     onSearchMovies(value)
-    setLoading(true)
+    setState(QueryState.LOADING)
   }
 
   return (
     <MoviesContext.Provider
-      value={{ query, movies, handleSubmit, updateQuery, loading }}
+      value={{ query, movies, handleSubmit, updateQuery, state }}
     >
       {children}
     </MoviesContext.Provider>
